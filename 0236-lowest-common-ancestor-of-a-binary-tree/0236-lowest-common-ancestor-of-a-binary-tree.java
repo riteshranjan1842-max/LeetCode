9c34c5be7de55843a0d83bcbe9b255a3a3557543
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-08 19:23:52 +0530

    Time: 15 ms (56.38%), Space: 69.8 MB (35.52%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -10,17 +10,27 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null) return root;
-        if(p.val==root.val || q.val==root.val) return root;
-        boolean pLeft = exists(root.left,p.val);
-        boolean qLeft = exists(root.left,q.val);
-        if(pLeft && qLeft) return lowestCommonAncestor(root.left,p,q);
-        else if(!pLeft && !qLeft) return lowestCommonAncestor(root.right,p,q);
-        else return root;
-
-    }
-    public boolean exists(TreeNode root, int val){
-        if(root==null) return false;
-        if(root.val==val) return true;
-        return exists(root.left,val) || exists(root.right,val);
+        if(p==root || q==root) return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q); 
+        TreeNode right = lowestCommonAncestor(root.right,p,q);      
+       if(left != null && right != null) return root;
+        else return left != null? left : right;
     }
 }
+
+// Method 1      // O(n) h tc abhi v
+//  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root==null) return root;
+//         if(p.val==root.val || q.val==root.val) return root;
+//         boolean pLeft = exists(root.left,p.val);
+//         boolean qRight = exists(root.left,q.val);
+//         if(pLeft && !qRight) return lowestCommonAncestor(root.left,p,q);
+//         else if(!pLeft && qRight) return lowestCommonAncestor(root.right,p,q);
+//         else return root;
+
+//     }
+//     public boolean exists(TreeNode root, int val){
+//         if(root==null) return false;
+//         if(root.val==val) return true;
+//         return exists(root.left,val) || exists(root.right,val);
+//     }
