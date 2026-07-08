7beb9e0fd70f2fbf9e6259999c977df57ded26ad
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-08 20:26:24 +0530

    Time: 0 ms (100%), Space: 44.1 MB (33.56%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -16,20 +16,37 @@
 class Solution {
     public void flatten(TreeNode root) {
         if(root==null) return;
-        ArrayList<TreeNode> arr = new ArrayList<>();
-        dfs(root,arr);
-        int n = arr.size();
-        for(int i = 0; i<n-1; i++){
-            TreeNode temp = arr.get(i);
-            temp.left = null;
-            temp.right =  arr.get(i+1);
-        }
-        arr.get(n-1).right = null;
+        TreeNode lst = root.left;
+        TreeNode rst = root.right;
+        root.left = null;
+        root.right = null;
+        flatten(lst);
+        flatten(rst);
+        root.right = lst;
+        TreeNode last = root;
+        while(last.right != null) last = last.right;
+        last.right = rst;
+        
     }
-    public void dfs(TreeNode root, ArrayList<TreeNode> arr){
-          if(root==null) return;
-          arr.add(root);
-          dfs(root.left,arr);
-          dfs(root.right,arr);
-    }
-}
+} 
+
+
+// Method 1
+// public void flatten(TreeNode root) {
+//         if(root==null) return;
+//         ArrayList<TreeNode> arr = new ArrayList<>();
+//         dfs(root,arr);
+//         int n = arr.size();
+//         for(int i = 0; i<n-1; i++){
+//             TreeNode temp = arr.get(i);
+//             temp.left = null;
+//             temp.right =  arr.get(i+1);
+//         }
+//         arr.get(n-1).right = null;
+//     }
+//     public void dfs(TreeNode root, ArrayList<TreeNode> arr){
+//           if(root==null) return;
+//           arr.add(root);
+//           dfs(root.left,arr);
+//           dfs(root.right,arr);
+//     }
