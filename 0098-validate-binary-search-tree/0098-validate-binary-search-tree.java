7a969575dd70e99a7fb984b4a99299d64a23399e
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-13 16:30:46 +0530

    Time: 1 ms (16.91%), Space: 46.7 MB (5.6%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,35 +1,47 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        inorder(root,ans);
-        int i = 0;
-        int j = i+1;
-        while(j<ans.size()){
-            if(ans.get(i)>=ans.get(j)) return false;
-            i++;
-            j = i+1;
-        }
-        return true;
-    }
-    public void inorder(TreeNode root, ArrayList<Integer> ans){
-        if(root==null) return;
-        inorder(root.left,ans);
-        ans.add(root.val);
-        inorder(root.right,ans);
+class Pair{
+    long max;
+    long min;
+    Pair(long max, long min){
+        this.max = max;
+        this.min = min;
     }
 }
+class Solution {
+    static boolean flag;
+    public boolean isValidBST(TreeNode root) {
+        if(root.left==null && root.right==null) return true;
+        flag = true;
+        maxMin(root);
+        return flag;
+    }
+    Pair maxMin(TreeNode root){
+        if(root==null) return new Pair(Long.MIN_VALUE, Long.MAX_VALUE);
+        Pair lst = maxMin(root.left);
+        Pair rst = maxMin(root.right);
+        long val = (long)(root.val);
+        long max = Math.max(val, Math.max(lst.max,rst.max));
+        long min = Math.min(val, Math.min(lst.min,rst.min));
+        if(root.val <= lst.max || root.val >= rst.min) flag = false;
+        return new Pair(max, min);
+    } 
+}
+
+// Method 1
+// public boolean isValidBST(TreeNode root) {
+//         ArrayList<Integer> ans = new ArrayList<>();
+//         inorder(root,ans);
+//         int i = 0;
+//         int j = i+1;
+//         while(j<ans.size()){
+//             if(ans.get(i)>=ans.get(j)) return false;
+//             i++;
+//             j = i+1;
+//         }
+//         return true;
+//     }
+//     public void inorder(TreeNode root, ArrayList<Integer> ans){
+//         if(root==null) return;
+//         inorder(root.left,ans);
+//         ans.add(root.val);
+//         inorder(root.right,ans);
+//     }
