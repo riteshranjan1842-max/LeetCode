5687c53a887be7ee1f80f91fbc8cd57e5128428d
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-15 22:01:07 +0530

    Time: 10 ms (45.53%), Space: 50.9 MB (44.62%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -15,17 +15,53 @@
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        helper(root.left, root.right, 1);
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<TreeNode>> ans = new ArrayList<>();
+        q.add(root);
+        while(q.size()>0){
+            int n = q.size();
+            List<TreeNode> arr = new ArrayList<>();
+            while(n>0){
+                TreeNode temp = q.remove();
+                arr.add(temp);
+                if(temp.left != null) q.add(temp.left);
+                if(temp.right != null) q.add(temp.right);
+                n--;
+            }
+            ans.add(arr);
+        }
+        for(int i = 0; i<ans.size(); i++){
+            List<TreeNode> arr = ans.get(i);
+            if(i%2==1){
+                int l = 0;
+                int r = arr.size()-1;
+                while(l < r){
+                    TreeNode left = arr.get(l);
+                    TreeNode right =arr.get(r);
+                   int temp = left.val;
+                   left.val = right.val;
+                   right.val = temp;
+                   l++;
+                   r--;
+                }
+            }
+        }
         return root;
     }
-    public void helper(TreeNode root1, TreeNode root2, int level){
-        if(root1==null || root2==null) return;
-        if(level%2 != 0){
-            int temp = root1.val;
-            root1.val = root2.val;
-            root2.val = temp;
-        }
-        helper(root1.left,root2.right, level+1);
-        helper(root1.right,root2.left, level+1);
-    }
 }
+
+// Method 1
+// public TreeNode reverseOddLevels(TreeNode root) {
+//         helper(root.left, root.right, 1);
+//         return root;
+//     }
+//     public void helper(TreeNode root1, TreeNode root2, int level){
+//         if(root1==null || root2==null) return;
+//         if(level%2 != 0){
+//             int temp = root1.val;
+//             root1.val = root2.val;
+//             root2.val = temp;
+//         }
+//         helper(root1.left,root2.right, level+1);
+//         helper(root1.right,root2.left, level+1);
+//     }
