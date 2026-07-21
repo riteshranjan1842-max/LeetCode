9d95582ebd7d08738852122d027cdc09b7acac36
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-21 13:06:10 +0530

    Time: 128 ms (82.55%), Space: 111.6 MB (38.22%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,54 +1,30 @@
-class MedianFinder { // we are creating a DS
+class MedianFinder {
     PriorityQueue<Integer> minHeap;
     PriorityQueue<Integer> maxHeap;
     public MedianFinder() {
-       minHeap = new PriorityQueue<>();
-       maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
     }
-
+    
     public void addNum(int num) {
-      if(maxHeap.size() == 0) maxHeap.add(num);
-      else{
-          if(num < maxHeap.peek()) maxHeap.add(num);
-          else minHeap.add(num);
-      }
-      if(maxHeap.size() == minHeap.size()+2)
-          minHeap.add(maxHeap.poll());
-
-      if(maxHeap.size()+2 == minHeap.size())
-          maxHeap.add(minHeap.poll());
-
-
+        if(maxHeap.size()==0) maxHeap.add(num);
+        else{
+            if(num < maxHeap.peek()) maxHeap.add(num);
+            else minHeap.add(num);
+        }
+        if(maxHeap.size()==minHeap.size()+2) minHeap.add(maxHeap.remove());
+        if(minHeap.size()==maxHeap.size()+2) maxHeap.add(minHeap.remove());
     }
-    // 1 2 3 4 5 6
+    
     public double findMedian() {
-        if(maxHeap.size() == minHeap.size()+1) return maxHeap.peek();
-        else if(maxHeap.size()+1 == minHeap.size()) return minHeap.peek();
-        else return (maxHeap.peek() + minHeap.peek())/2.0;
+        if(minHeap.size() == maxHeap.size()+1) return (double)minHeap.peek();
+        else if(minHeap.size()+1 == maxHeap.size()) return (double)maxHeap.peek();
+        else{
+            return (minHeap.peek() + maxHeap.peek())/2.0;
+        }
     }
 }
 
-//Method 1 : TLE
-// class MedianFinder { // we are creating a DS
-//     ArrayList<Integer>  arr;
-//     public MedianFinder() {
-//         arr = new ArrayList<>();
-//     }
-
-//     public void addNum(int num) {
-//        arr.add(num);
-//     }
-//     // 1 2 3 4 5 6 
-//     public double findMedian() {
-//         Collections.sort(arr);
-//        int n = arr.size();
-//        if(n%2==1)
-//        {
-//            return (double)arr.get(n/2);
-//        }
-//        else return (double)(arr.get(n/2-1) + arr.get(n/2))/2;
-//     }
-// }
 /**
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder obj = new MedianFinder();
