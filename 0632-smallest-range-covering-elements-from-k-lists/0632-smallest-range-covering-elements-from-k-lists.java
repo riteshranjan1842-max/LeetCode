4c55ba82dc9149d60fa71d666471e0743d47365b
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-21 13:28:05 +0530

    Time: 102 ms (36.25%), Space: 86.5 MB (22.25%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -2,35 +2,35 @@ class Triplet implements Comparable<Triplet>{
     int ele;
     int row;
     int col;
-    public Triplet(int ele, int row, int col) {
+    Triplet(int ele, int row, int col){
         this.ele = ele;
         this.row = row;
         this.col = col;
     }
-    @Override
-    public int compareTo(Triplet t) {
+    public int compareTo(Triplet t){
         return this.ele - t.ele;
     }
 }
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-       int[] ans = new int[2];
         PriorityQueue<Triplet> pq = new PriorityQueue<>();
-        int k = nums.size();
-        int max =  Integer.MIN_VALUE;
+        int[] ans = new int[2];
+        int k = nums.size(); // rows
+        int n = nums.get(0).size(); // columns;
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < k; i++) {
-            max = Math.max(max, nums.get(i).get(0));
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<k; i++){
             min = Math.min(min, nums.get(i).get(0));
+            max = Math.max(max, nums.get(i).get(0));
             pq.add(new Triplet(nums.get(i).get(0), i, 0));
         }
-        int a = min, b = max; // [a,b] is the range
+        int a = min, b = max;
         while(true){
-            Triplet top = pq.poll();
-            int ele = top.ele;
-            int row = top.row;
-            int col = top.col;
-            if(max-ele < b-a){
+            Triplet t = pq.remove();
+            int ele = t.ele;
+            int row = t.row;
+            int col = t.col;
+            if(max - ele < b-a){
                 a = ele;
                 b = max;
             }
@@ -40,7 +40,7 @@ class Solution {
             pq.add(new Triplet(next, row, col+1));
         }
         ans[0] = a;
-        ans[1] = b; 
+        ans[1] = b;
         return ans; 
     }
 }
