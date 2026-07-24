c864167dd472f52456c704515a982863cb2056a6
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-24 20:14:29 +0530

    Time: 0 ms (100%), Space: 46.5 MB (52.32%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -2,61 +2,22 @@ class Solution {
     public Node copyRandomList(Node head1) {
         // Step 1: Creating Deep Copy without Random
         Node head2 = deepCopy(head1);
-        // Step[ 2: Alternative Connect both LL
-        merge(head1, head2);
-        // Step 3 : Assingning a Random Pointer
-        randomConnection(head1,head2);
-
-        //Step 4: Spiltting the combine list
-        split(head1);
-        
+        HashMap<Node, Node> map = new HashMap<>();
+        Node temp1 = head1;
+        Node temp2 = head2;
+        map.put(null, null);
+        while(temp1 != null){
+            map.put(temp1, temp2);
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        temp1 = head1;
+        while(temp1 != null){
+            temp2 = map.get(temp1);
+            temp2.random = map.get(temp1.random);
+            temp1 = temp1.next;
+        }
         return head2;
-    }
-    public void split(Node head1){
-        Node dummy1 = new Node(-1);
-        Node temp1 = dummy1;
-        Node dummy2 = new Node(-1);
-        Node temp2 = dummy2;
-        Node temp = head1;   // merge list pe traverse
-        while(temp != null){
-            temp1.next = temp;
-            temp1 = temp1.next;
-            temp = temp.next;
-
-            temp2.next = temp;
-            temp2 = temp2.next;
-            temp = temp.next;
-        }
-        temp1.next = null;
-        head1 = dummy2.next;
-
-    }
-    public void randomConnection(Node head1, Node head2){
-        Node temp1 = head1;
-        Node temp2 = head2;
-        while(temp1 != null && temp2 != null){
-            if(temp1.random == null) temp2.random = null;
-            else temp2.random = temp1.random.next;
-
-            temp1 = temp1.next.next;
-            if(temp2.next != null) temp2 = temp2.next.next;
-        }
-    }
-    public void merge(Node head1, Node head2){
-        Node dummy = new Node(-1);
-        Node temp = dummy;
-        Node temp1 = head1;
-        Node temp2 = head2;
-        while(temp2 != null){
-            temp.next = temp1;
-            temp = temp.next;
-            temp1 = temp1.next;
-
-            temp.next = temp2;
-            temp = temp.next;
-            temp2 = temp2.next;
-        }
-        head1 = dummy.next;
     }
     public Node deepCopy(Node head1){
         Node temp1 = head1;
@@ -71,3 +32,6 @@ class Solution {
          return head2.next;
     }
 }
+
+
+
