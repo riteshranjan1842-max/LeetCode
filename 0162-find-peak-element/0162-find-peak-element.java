c209032fc1f7f71f66d1f6a3a4d9ebf5aff18ad5
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-28 19:26:40 +0530

    Time: 0 ms (100%), Space: 44 MB (74.82%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,8 +1,23 @@
 class Solution {
     public int findPeakElement(int[] arr) {
-        for(int i = 0; i<arr.length; i++){
-            if((i==0 || arr[i-1]<arr[i]) && (i==arr.length-1 || arr[i]>arr[i+1])) return i;
+       // Method 2 : Binary Search
+       if(arr.length==1) return 0;
+       int n = arr.length;
+       if(arr[0]>arr[1]) return 0;
+       if(arr[n-1]>arr[n-2]) return n-1;
+       int low = 1, high = n-2;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if((arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])) return mid;
+            else if(arr[mid-1]<arr[mid]) low = mid + 1;
+            else high = mid - 1;
         }
         return -1;
     }
 }
+
+// Method 1 : O(n)
+//  for(int i = 0; i<arr.length; i++){
+//             if((i==0 || arr[i-1]<arr[i]) && (i==arr.length-1 || arr[i]>arr[i+1])) return i;
+//         }
+//         return -1;
