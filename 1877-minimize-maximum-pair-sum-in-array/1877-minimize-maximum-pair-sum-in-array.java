41ceb09c778ed8d270e66e9e94de9e7e5bc8ea1d
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-29 16:47:38 +0530

    Time: 58 ms (67.87%), Space: 97.2 MB (8.87%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -1,16 +1,27 @@
 class Solution {
     public int minPairSum(int[] nums) {
-        PriorityQueue<Integer> min = new PriorityQueue<>();
-        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+        Arrays.sort(nums);
         int n = nums.length;
-        int maxSum = Integer.MIN_VALUE;
-        for(int ele : nums){
-            min.add(ele);
-            max.add(ele);
+        int i = 0, j = n-1;
+        int max = Integer.MIN_VALUE;
+        while(i<=j){
+            max = Math.max(max, nums[i]+nums[j]);
+            i++;
+            j--;
         }
-        for(int i = 0; i<=n/2; i++){
-            maxSum = Math.max(min.remove()+max.remove(), maxSum);
-        }
-        return maxSum;
+        return max;
     }
 }
+// Method 1
+// PriorityQueue<Integer> min = new PriorityQueue<>();
+//         PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+//         int n = nums.length;
+//         int maxSum = Integer.MIN_VALUE;
+//         for(int ele : nums){
+//             min.add(ele);
+//             max.add(ele);
+//         }
+//         for(int i = 0; i<=n/2; i++){
+//             maxSum = Math.max(min.remove()+max.remove(), maxSum);
+//         }
+//         return maxSum;
