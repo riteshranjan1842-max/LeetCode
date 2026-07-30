8435d66ed2cd674c46096af9ee52f12fb9cd59db
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-30 12:00:47 +0530

    Time: 9 ms (43.39%), Space: 41.8 MB (93.78%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int fib(int n) {
+       return helper(n);
+    }
+    public int helper(int n) {
+       if(n==0 || n==1) return n;
+       return helper(n-1)+helper(n-2); 
+    }
+}
