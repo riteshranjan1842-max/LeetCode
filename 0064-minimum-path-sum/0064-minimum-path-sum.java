4f970891b95b60ce4a46ab6c64d76eb623f6ea68
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-07-31 16:00:59 +0530

    Time: 4 ms (63.07%), Space: 50.3 MB (39.01%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,12 +1,30 @@
 class Solution {
+    //Tabulation Method
     public int minPathSum(int[][] grid) {
-        int dp[][] = new int[grid.length][grid[0].length];
-        return helper(0, 0, grid,dp);
+        int m = grid.length;
+        int n = grid[0].length;
+        int dp[][] = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                   if(i==0 && j==0) dp[i][j] = grid[i][j];
+                   else if(i==0) dp[i][j] = grid[i][j] + dp[i][j-1];
+                   else if(j==0)  dp[i][j] = grid[i][j] + dp[i-1][j];
+                    else dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
     }
-    public int helper(int i, int j, int[][] grid, int[][] dp) {
-        if(i== grid.length-1 && j == grid[0].length-1) return grid[i][j];
-        if(i==grid.length || j==grid[0].length) return Integer.MAX_VALUE;
-        if(dp[i][j]!=0) return dp[i][j];
-        return dp[i][j] = grid[i][j] + Math.min(helper(i+1, j, grid, dp), helper(i, j+1, grid, dp));
-    }
+
+
+    // Memorization Method
+    // public int minPathSum(int[][] grid) {
+    //     int dp[][] = new int[grid.length][grid[0].length];
+    //     return helper(0, 0, grid,dp);
+    // }
+    // public int helper(int i, int j, int[][] grid, int[][] dp) {
+    //     if(i== grid.length-1 && j == grid[0].length-1) return grid[i][j];
+    //     if(i==grid.length || j==grid[0].length) return Integer.MAX_VALUE;
+    //     if(dp[i][j]!=0) return dp[i][j];
+    //     return dp[i][j] = grid[i][j] + Math.min(helper(i+1, j, grid, dp), helper(i, j+1, grid, dp));
+    // }
 }
