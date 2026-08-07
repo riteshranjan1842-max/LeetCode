e0fa30488701672c8dcd84b943e3c23555ec152a
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-08-07 15:23:43 +0530

    Time: 26 ms (79.04%), Space: 44.2 MB (86.52%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,22 +1,43 @@
 class Solution {
+    // Tabulation Method
     public int numSquares(int n) {
        int[] dp = new int[n+1];
-       Arrays.fill(dp, -1);
-       return counting(n, dp);
+       for(int i = 1; i<=n; i++){
+          if(isPerfect(i)) dp[i] = 1;
+          else{
+            int min = n;
+               for(int j = 1; j*j<=i; j++){
+                   int count = dp[j*j] + dp[i-j*j];
+                   min = Math.min(min, count);
+               }
+               dp[i] = min;
+           }
+       }
+       return dp[n];
     }
-    public int counting(int n, int[] dp){
-         if(isPerfect(n)) return 1;
-         if(dp[n]!=-1) return dp[n];
-         int min = n;
-         int sqrt = (int)Math.ceil((Math.sqrt(n)));
-        for(int i = 1; i*i<=n; i++){
-            int count = counting(i*i, dp) + counting(n-i*i, dp);
-            min = Math.min(min, count);
-        }
-        return dp[n] = min;
-    }
-    public boolean isPerfect(int n){
+      public boolean isPerfect(int n){
         int sqrt = (int)(Math.sqrt(n));
         return sqrt*sqrt == n;
     }
 }
+
+// Memoization Method
+// public int numSquares(int n) {
+//        int[] dp = new int[n+1];
+//        Arrays.fill(dp, -1);
+//        return counting(n, dp);
+//     }
+//     public int counting(int n, int[] dp){
+//          if(isPerfect(n)) return 1;
+//          if(dp[n]!=-1) return dp[n];
+//          int min = n;
+//         for(int i = 1; i*i<=n; i++){
+//             int count = counting(i*i, dp) + counting(n-i*i, dp);
+//             min = Math.min(min, count);
+//         }
+//         return dp[n] = min;
+//     }
+//     public boolean isPerfect(int n){
+//         int sqrt = (int)(Math.sqrt(n));
+//         return sqrt*sqrt == n;
+//     }
