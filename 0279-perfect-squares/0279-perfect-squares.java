edb8ee0d66cd506e6621c610be9d8b4eb51eb9a1
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-08-07 15:03:00 +0530

    Time: 140 ms (14.93%), Space: 45.3 MB (19.33%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSquares(int n) {
+       int[] dp = new int[n+1];
+       Arrays.fill(dp, -1);
+       return counting(n, dp);
+    }
+    public int counting(int n, int[] dp){
+         if(isPerfect(n)) return 1;
+         if(dp[n]!=-1) return dp[n];
+         int min = n;
+         int sqrt = (int)Math.ceil((Math.sqrt(n)));
+        for(int i = 1; i*i<=n; i++){
+            int count = counting(i*i, dp) + counting(n-i*i, dp);
+            min = Math.min(min, count);
+        }
+        return dp[n] = min;
+    }
+    public boolean isPerfect(int n){
+        int sqrt = (int)(Math.sqrt(n));
+        return sqrt*sqrt == n;
+    }
+}
