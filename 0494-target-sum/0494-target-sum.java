31834ae2237f0120220bd0b9937855181061dce0
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-08-16 13:57:12 +0530

    Time: 285 ms (32.92%), Space: 47.1 MB (14.09%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,13 +1,21 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        return helper(0, nums, target);
+    static int absSum;
+    public  int findTargetSumWays(int[] nums, int target) {
+        absSum = 0;
+        for(int ele : nums){
+            absSum += Math.abs(ele);
+        }
+        int dp[][] = new int[nums.length][2*absSum+1];
+        return helper(0, 0, nums, target, dp);
     }
-    public int helper(int i,  int[] nums, int target){
+    // i -> i to 0     sum -> sum to target-arrayAbsSum
+    public int helper(int i,  int sum , int[] nums, int target, int[][] dp){
         if(i==nums.length){
-            return (target==0) ? 1 : 0;
-        } 
-        int add = helper(i+1, nums, target-nums[i]);
-        int sub = helper(i+1, nums, target+nums[i]);
-        return add + sub;
+            return (target==sum) ? 1 : 0;
+        }
+        if(dp[i][sum+absSum]!=0) return dp[i][sum+absSum];
+        int add = helper(i+1,sum-nums[i], nums, target, dp);
+        int sub = helper(i+1, sum+nums[i], nums, target,dp);
+        return dp[i][sum+absSum] = add + sub;
     }
 }
