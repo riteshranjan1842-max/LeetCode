5aa69a507f2afd96f24f8979dcd07a22d1aa649f
Ritesh Ranjan <riteshranjan1842@gmail.com>
2026-09-23 19:07:55 +0530

    Time: 1 ms (55.11%), Space: 47.2 MB (8.8%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -2,25 +2,34 @@ class Solution {
     public int countNegatives(int[][] grid) {
         int count = 0;
         for(int i = 0; i<grid.length; i++){
-            Arrays.sort(grid[i]);
-            
+            for(int j = 0; j<grid[0].length; j++){
+                if(grid[i][j]<0) count++;
+            }
         }
-        for(int i = 0; i<grid.length; i++){
-           count += binarySearch(grid[i]);
-           }
         return count;
     }
-    public int binarySearch(int[] grid){
-        int low = 0, high = grid.length-1;
-        int neg = 0;
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(grid[mid]<0){
-                low = mid + 1;
-                neg = low;
-            } 
-            else high = mid - 1;
-        }
-       return  (neg == -1) ? 0 : neg ;
-    }
+    // public int countNegatives(int[][] grid) {
+    //     int count = 0;
+    //     for(int i = 0; i<grid.length; i++){ 
+    //         Arrays.sort(grid[i]);
+            
+    //     }
+    //     for(int i = 0; i<grid.length; i++){
+    //        count += binarySearch(grid[i]);
+    //        }
+    //     return count;
+    // }
+    // public int binarySearch(int[] grid){
+    //     int low = 0, high = grid.length-1;
+    //     int neg = 0;
+    //     while(low <= high){
+    //         int mid = (low + high)/2;
+    //         if(grid[mid]<0){
+    //             low = mid + 1;
+    //             neg = low;
+    //         } 
+    //         else high = mid - 1;
+    //     }
+    //    return  (neg == -1) ? 0 : neg ;
+    // }
 }
